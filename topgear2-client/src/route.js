ba4e10700c9da4831363aec719aa6d8531f94684
jaredjames2020 <jaredjames2020@gmail.com>
2020-05-18 16:21:10 -0400

    route class constructor

--- topgear2-client/src/route.js
+++ topgear2-client/src/route.js
@@ -1,5 +1,10 @@
 class Route {
-    constructor(name){
-      this.name = name;
+    constructor(bus_line_name, service_stops, service_status, service_origin, service_destination) {
+      this.bus_line_name = bus_line_name;
+      this.service_stops = service_stops;
+      this.service_status = service_status;
+      this.service_origin = service_origin;
+      this.service_destination = service_destination;
+      AppContainer.route.push(this);
     }
 }
